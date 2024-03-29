8e0b020ee4f254b853d81cfaeba0c4d13a0fa05e
RuthAdora <adoraruth01@hmail.com>
2024-03-29 09:51:12 +0300

    initianl commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const products = [ //Array
+  { title: 'Cabbage', isFruit: false, id: 1 },
+  { title: 'Garlic', isFruit: false, id: 2 },
+  { title: 'Apple', isFruit: true, id: 3 },
+  { title: 'Carrots', isFruit: true, id: 4 },
+  { title: 'kales', isFruit: false, id: 5 },
+];
+export default function ShoppingList() { //main component
+  const listItems = products.map(product => // products - a constant array. (.map) iterates over each item in the array and applie a function
+    <li //creates a list item element
+    key ={product.id} //assigns a unique key to each list item
+    style={{ // style attribute
+      color: product.isFruit ? 'magenta' : 'darkgreen'
+    }}
+    // {product.title} - accesses the title property for the current product object
+    // {listItems} - inserts the item array which nowcontains the jsx element for each list item
+    >
+      {product.title} 
+      </li>
+      );
 
-export default App;
+      return ( // returns jsx code that displays the list
+        <ul>{listItems}</ul>
+      );
+}
